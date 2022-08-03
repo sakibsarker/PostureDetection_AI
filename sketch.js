a2d64394b6dd1c718b30bf45bfb356b3195c772a
sakibsarker <sakib15-3824@diu.edu.bd>
2022-08-03 20:03:10 +0600

    added

--- sketch.js
+++ sketch.js
@@ -36,7 +36,7 @@ function modelLoaded() {
 
 function draw() {
 
-    // images and videos(webcam)
+
     image(capture, 0, 0);
     fill(255,0,0);
 
@@ -50,8 +50,7 @@ function draw() {
             line(skeleton[j][0].position.x, skeleton[j][0].position.y, skeleton[j][1].position.x, skeleton[j][1].position.y)
         }
 
-        //image(specs,singlePose.nose.x-35,singlePose.nose.y-50,80,80);
-        //image(smoke,singlePose.nose.x-35,singlePose.nose.y+10,40,40);
+       
 
         
     }
